8349b77d0bdf39260da8f8a4b983f99aadf0c4f0
Mark McCormack <th4tirishguy@gmail.com>
2017-04-18 03:06:01 +0100

    added more users to json file

--- app_api/controllers/users.js
+++ app_api/controllers/users.js
@@ -5,7 +5,9 @@ var data = [
       "loc": {
         "x": -8.614616,
         "y": 51.923562
-      }
+      },
+      "campus": "CIT Bishopstown",
+      "currentStatus": "Passenger"
 		},
     {
 			"id": 2,
@@ -13,7 +15,9 @@ var data = [
       "loc": {
         "x": -8.399520,
         "y": 51.920015
-      }
+      },
+      "campus": "CIT Bishopstown",
+      "currentStatus": "Passenger"
 		},
     {
 			"id": 3,
@@ -21,7 +25,9 @@ var data = [
       "loc": {
         "x": -8.531702,
         "y": 51.707459
-      }
+      },
+      "campus": "CIT Crawford",
+      "currentStatus": "Passenger"
 		},
     {
 			"id": 4,
@@ -29,8 +35,50 @@ var data = [
       "loc": {
         "x": -8.962285,
         "y": 51.909404
-      }
-		}
+      },
+      "campus": "CIT School of Music",
+      "currentStatus": "Passenger"
+		},
+    {
+      "id": 5,
+      "name": "John",
+      "loc": {
+        "x": -8.286267,
+        "y": 52.132275
+      },
+      "campus": "CIT School of Music",
+      "currentStatus": "Passenger"
+    },
+    {
+      "id": 6,
+      "name": "Hannah",
+      "loc": {
+        "x": -8.383161,
+        "y": 51.815982
+      },
+      "campus": "CIT School of Music",
+      "currentStatus": "Passenger"
+    },
+    {
+      "id": 7,
+      "name": "Ryan",
+      "loc": {
+        "x": -8.288213,
+        "y": 51.805610
+      },
+      "campus": "CIT School of Music",
+      "currentStatus": "Passenger"
+    },
+    {
+      "id": 8,
+      "name": "Andrew",
+      "loc": {
+        "x": -8.961634,
+        "y": 51.909912
+      },
+      "campus": "CIT School of Music",
+      "currentStatus": "Passenger"
+    }
 	]
 
 module.exports.index = function(req, res) {
